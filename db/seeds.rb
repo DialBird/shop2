DialBird/shop_gokko

seedにuserを追加する

db/seeds.rb
@@ -1,3 +1,7 @@
+# user
+User.create(email: 'rememberthatk@icloud.com', password: 'password')
+
+# product
 Product.create(name: 'りんご', price: 200, cost: 100, image: File.open("#{Rails.root}/db/data/apple.jpg"))
 Product.create(name: 'バナナ', price: 100, cost: 50, image: File.open("#{Rails.root}/db/data/banana.jpg"))
 Product.create(name: 'ぶどう', price: 500, cost: 20, image: File.open("#{Rails.root}/db/data/grape.jpg"))

DialBird/shop2

db/seeds.rb
@@ -1,3 +1,7 @@
+# user
+User.create(email: 'tekitou@co.jp', password: 'password')
+
+# product
 Product.create(name: 'りんご', price: 200, cost: 100, image: File.open("#{Rails.root}/db/data/apple.jpg"))
 Product.create(name: 'バナナ', price: 100, cost: 50, image: File.open("#{Rails.root}/db/data/banana.jpg"))
 Product.create(name: 'ぶどう', price: 500, cost: 20, image: File.open("#{Rails.root}/db/data/grape.jpg"))