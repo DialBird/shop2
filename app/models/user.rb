6876513a15fbd3210bb029cffc5b81046d8fb496
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-02 12:37:36 +0000

    userモデルのcartをhas_oneにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :carts
+  has_one :cart
 end
