d19d9d1b67ed0b94e67b6d2aed34884a5009f881
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-27 07:40:26 +0000

    simplecovの設定をspec_helperに追加する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,13 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+# simplecov
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails'
+
 require 'rspec/rails'
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
